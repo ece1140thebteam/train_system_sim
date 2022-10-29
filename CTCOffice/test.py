03257f298f4e5e090480504539fd9c85a21b57d0
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2022-10-29 18:50:45 -0400

    update test casese to only run once

--- CTCOffice/test.py
+++ CTCOffice/test.py
@@ -10,33 +10,28 @@ import sys
 
 
 class TestCtc(unittest.TestCase):
-    def test_throughput(self):
+    @classmethod
+    def setUpClass(self):
         app = QApplication(sys.argv)
-        window = MainWindow()
+        self.window = MainWindow()
 
-        window.update_throughput("Red", 100)
-        self.assertEqual(window.throughputs[0].get_throughput(), 100)
+    def test_throughput(self):
+        self.window.update_throughput("Red", 100)
+        self.assertEqual(self.window.throughputs[0].get_throughput(), 100)
 
-        window.update_throughput("Green", 200)
-        self.assertEqual(window.throughputs[1].get_throughput(), 200)
+        self.window.update_throughput("Green", 200)
+        self.assertEqual(self.window.throughputs[1].get_throughput(), 200)
 
     def test_failure(self):
-        app = QApplication(sys.argv)
-        window = MainWindow()
-
-        window.update_failure("Red", 1, "Power Failure")
-
-        self.assertEqual(window.lines[0].get(1).failure, "Power Failure")
+        self.window.update_failure("Red", 1, "Power Failure")
+        self.assertEqual(self.window.lines[0].get(1).failure, "Power Failure")
 
     def test_crossing(self):
-        app = QApplication(sys.argv)
-        window = MainWindow()
+        self.window.update_crossing("Red", 1, 0)
+        self.assertEqual(self.window.lines[0].get(1).crossing, 0)
 
-        window.update_crossing("Red", 1, 0)
-        self.assertEqual(window.lines[0].get(1).crossing, 0)
-
-        window.update_crossing("Red", 1, 1)
-        self.assertEqual(window.lines[0].get(1).crossing, 1)
+        self.window.update_crossing("Red", 1, 1)
+        self.assertEqual(self.window.lines[0].get(1).crossing, 1)
 
 if __name__ == '__main__':
     unittest.main()
