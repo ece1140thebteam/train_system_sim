016ea4ac27d3c54041b793dee29658e308e24871
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2022-11-04 16:10:14 -0400

    add howto to signals class

--- signals.py
+++ signals.py
@@ -3,6 +3,12 @@ from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
 class signals(QObject):
+  # How to use signals
+  #
+  # Add import to file `from signals import s`
+  #
+  # See "CTCOffice/main.py" line 98 to see how to connect signal output
+  # See "CTCOffice/testUiMain.py" line 180 to see how to emit signal 
   send_throughput_signal = pyqtSignal(str, int)
 
 s = signals()
