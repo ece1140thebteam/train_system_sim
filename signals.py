1084427f1a7e273a5879e7294d8e496a6eccdadf
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2022-11-07 20:38:48 -0500

    update signal types

--- signals.py
+++ signals.py
@@ -14,25 +14,25 @@ class signals(QObject):
   send_TrackModel_throughput_signal = pyqtSignal(str, int)
 
   # Send switch position from CTC to Track Controller
-  send_CTC_switch_position_signal = pyqtSignal(str, int, int) # Line, block, position (0=normal, 1=reverse)
+  send_CTC_switch_position_signal = pyqtSignal(str, list) # Line, block, position (0=normal, 1=reverse)
 
   # Send maintenance mode from CTC to Track Controller
-  send_CTC_maintenance_mode_signal = pyqtSignal(str, int, int) # Line, block, mode (0=disabled, 1=enables)
+  send_CTC_maintenance_mode_signal = pyqtSignal(str, list) # Line, block, mode (0=disabled, 1=enables)
 
   # Send suggested speed from CTC to Track Controller
-  send_CTC_suggested_speed = pyqtSignal(str, int, int) # Line, block, speed
+  send_CTC_suggested_speed = pyqtSignal(str, list) # Line, block, speed
 
   # Send track occupancy from Track Controller to CTC
-  send_TrackController_track_occupancy = pyqtSignal(str, int, int) # Line, block, occupancy (0=open, 1=occupied)
+  send_TrackController_track_occupancy = pyqtSignal(str, list) # Line, block, occupancy (0=open, 1=occupied)
 
   # Send block authority from CTC to Track Controller
-  send_CTC_authority = pyqtSignal(str, int, int) # Line, block, authority (0=no, 1=yes)
+  send_CTC_authority = pyqtSignal(str, list) # Line, block, authority (0=no, 1=yes)
 
   # Send crossing status from Track Controller to CTC
-  send_TrackController_crossing = pyqtSignal(str, int, int) # Line, block, status (0=deactivated, 1=activated)
+  send_TrackController_crossing = pyqtSignal(str, list) # Line, block, status (0=deactivated, 1=activated)
 
   # Send track failure from Track Controller to CTC
-  send_TrackController_failure = pyqtSignal(str, int, str) # Line, block, failure type
+  send_TrackController_failure = pyqtSignal(str, list) # Line, block, failure type
 
 
 # Create a single instance of this class to be used in all modules. 
