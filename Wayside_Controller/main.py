58f4ef443669e63242d758715eec35ecb520b7c8
ssraible@gmail.com <ssraible@gmail.com>
2022-11-17 02:51:27 -0500

    connect signals to ctc office and track model

--- Wayside_Controller/main.py
+++ Wayside_Controller/main.py
@@ -20,6 +20,18 @@ class MainWindow(QMainWindow, WaysideMainUI.Ui_MainWindow):
 
       # Upload PLC button connections
       self.uploadPLC1.clicked.connect(self.getFile1)
+      s.send_TrackModel_track_occupancy.connect(self.update_block_occupancy)
+
+      s.send_CTC_switch_position_signal.connect(self.ctc_set_switch_position) # [{'line':line, 'block':block, 'switch':0}]
+
+      # Send maintenance mode from CTC to Track Controller
+      s.send_CTC_maintenance_mode_signal.connect(self.ctc_set_maintenance_mode) # [{'line':line, 'block':block, 'mode':0}]
+
+      # Send suggested speed from CTC to Track Controller
+      s.send_CTC_suggested_speed.connect(self.ctc_set_suggested_speed) # [{'line':line, 'block':block, 'speed':0}]
+
+      # Send block authority from CTC to Track Controller
+      s.send_CTC_authority.connect(self.ctc_set_authority) # [{'line':line, 'block':block, 'authority':0}]
 
       # Allocate blocks with corresponding controller
       for line in track_info:
@@ -45,7 +57,90 @@ class MainWindow(QMainWindow, WaysideMainUI.Ui_MainWindow):
             
       #s.timer_tick.connect(self.handle_time_increment)
       #s.send_CTC_switch_position_signal.connect(self.update_switch_position)
+   def ctc_set_switch_position(self, updates_list):
+      controllers_to_update = []
+      for update in updates_list:
+         line = update['line']
+         block = update['block']
+         sw = update['switch']
+         track_info[line][block]['switch'] = sw
 
+         controller = track_info[line][block]['controller']
+         if controller not in controllers_to_update: 
+            controllers_to_update.append(controller)
+
+         s.send_TrackController_switch_pos.emit(line, block, sw)
+
+      for controller in controllers_to_update:
+         self.run_controllerx(controller)
+
+
+   def ctc_set_maintenance_mode(self, updates_list):
+      controllers_to_update = []
+      for update in updates_list:
+         line = update['line']
+         block = update['block']
+         m = update['mode']
+
+         track_info[line][block]['maintenance'] = m
+         
+         controller = track_info[line][block]['controller']
+         if controller not in controllers_to_update: 
+            controllers_to_update.append(controller)
+         print(m)
+         s.send_TrackModel_maintenance_status.emit(line, block, m==1)
+         
+      for controller in controllers_to_update:
+         self.run_controllerx(controller)
+
+   def ctc_set_suggested_speed(self, updates_list):
+      controllers_to_update = []
+      for update in updates_list:
+         line = update['line']
+         block = update['block']
+         s = update['speed']
+
+         controller = track_info[line][block]['controller']
+         if controller not in controllers_to_update: 
+            controllers_to_update.append(controller)
+
+         track_info[line][block]['suggested_speed'] = s
+
+      for controller in controllers_to_update:
+         self.run_controllerx(controller)
+
+   def ctc_set_authority(self, updates_list):
+      controllers_to_update = []
+      print(updates_list)
+      for update in updates_list:
+         line = update['line']
+         block = update['block']
+         auth = update['authority']
+
+         controller = track_info[line][block]['controller']
+         if controller not in controllers_to_update: 
+            controllers_to_update.append(controller)
+
+         track_info[line][block]['authority'] = auth
+
+      for controller in controllers_to_update:
+         self.run_controllerx(controller)
+
+   def run_controllerx(self, controller_num):
+      print(f'running wayside controller {controller_num}')
+      #TODO IMPLEMENT THE DIFFERENT CONTROLLERS
+
+   def update_block_occupancy(self, line, block, occupancy):
+      track_info[line][block]['occupancy'] = occupancy
+
+      self.run_controllerx(track_info[line][block]['controller'])
+      print('track controller occupancy updated')
+   
+   def update_authority(self, line, block, authority):
+      track_info[line][block]['authority'] = authority
+
+      self.run_controllerx(track_info[line][block]['controller'])
+      print('track controller authority updated')
 
    def update_switch_position(self, list):
       pass
