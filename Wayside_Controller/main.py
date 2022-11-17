734bb30d953c5b78d55856473f3d6749d23fa43a
CassandraOliva <cassandraoliva18@gmail.com>
2022-11-16 21:15:25 -0500

    Added Wayside Test UI

--- Wayside_Controller/main.py
+++ Wayside_Controller/main.py
@@ -18,12 +18,28 @@ class MainWindow(QMainWindow, WaysideMainUI.Ui_MainWindow):
 
       # Upload PLC button connections
       self.uploadPLC1.clicked.connect(self.getFile1)
+      self.track_info = {
+         'Green': {
+            1: { 'authority': 0, 'occupancy': 0, 'maintenance': False, 'suggested_speed': 0, 'track_failure': False, 'switch_pos': None}
+         },
+         'Red': {
+            1: { 'authority': 0, 'occupancy': 0, 'maintenance': False, 'suggested_speed': 0, 'track_failure': False, 'switch_pos': None}
+         }
+      }
+
+      gree_b1_auth = self.track_info['Green'][1]['authority']
+
+      for line in self.track_info:
+         print(line)
+         for block in self.track_info[line]:
+            print(self.track_info[line][block])
+            
       s.timer_tick.connect(self.handle_time_increment)
       s.send_CTC_switch_position_signal.connect(self.update_switch_position)
 
    def update_switch_position(self, line:str, positions:list):
       pass
-   
+
    def handle_time_increment(self):
       print('hi')
 
--- system.py
+++ system.py
@@ -5,6 +5,8 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
+from signals import s
+
 from CTCOffice.testUiMain import MainTestWindow as MainTestWindowCTC
 from CTCOffice.main import MainWindow as MainWindowCTC
 from TrainController.TrainCTRLui.ui_driver.mainwindow import TrainController
@@ -12,8 +14,6 @@ from TrainController.TrainCTRLui.ui_driver.trainctrltestui import TrainCTRLTestU
 from TrainController.TrainCTRLui.ui_driver.engineerui import engineerUI
 from track_model.track_model_qc.widget import TrackModel as TrackModelGUI
 from track_model.TrackModelTestUI.trackmodeltestui import TrackModelTestUI as TrackModelTestUI
- 
-from signals import s
 from Wayside_Controller.main import MainWindow as TrackControllerWindow
 from Train_Model.TrainModel import TrainModel, TestTrainModel
 
@@ -22,28 +22,39 @@ class SystemWindow(QMainWindow, system.Ui_MainWindow):
     def __init__(self, parent=None):
       super(SystemWindow, self).__init__(parent)
       self.setupUi(self)
-      
-      self.trackController = TrackControllerWindow()
-      self.trainmodel = TrainModel()
-      self.trainmodel_test = TestTrainModel(self.trainmodel)
-      self.pushButton_ctc.clicked.connect(self.open_CTC)
-      self.pushButton_ctc_test.clicked.connect(self.open_CTC_test)
-      self.pushButton_trainmodel.clicked.connect(self.open_trainmodel)
-      self.pushButton_trainmodel_test.clicked.connect(self.open_trainmodel_test)
 
       #Create list of trains for indexing
       self.Trains = []
 
-
-      self.pushButton_traincontrol.clicked.connect(self.open_traincontrol)
-      self.pushButton_traincontrol_test.clicked.connect(self.open_traincontrol_test)
+      # CTC variables
       self.main_windowCTC = MainWindowCTC()
       self.test_windowCTC = MainTestWindowCTC()
       self.pushButton_ctc.clicked.connect(self.open_CTC)
       self.pushButton_ctc_test.clicked.connect(self.open_CTC_test)
 
-      self.pushButton_start.clicked.connect(self.pause_timer)
+      # Track Controller Variables
+      self.trackController = TrackControllerWindow()
+      self.pushButton_trackcontrol.clicked.connect(self.open_TrackController)
+      # self.pushButton_trackcontrol_test.clicked.connect(self.open_TrackController_test)
 
+      # Track Model Variables
+      self.trackmodel = TrackModelGUI()
+      self.trackModelTestUI = TrackModelTestUI()
+      self.pushButton_trackmodel.clicked.connect(self.open_TrackModel)
+      self.pushButton_trackmodel_test.clicked.connect(self.open_TrackModelTestUI)
+
+      # Train Model Variables
+      self.trainmodel = TrainModel()
+      self.trainmodel_test = TestTrainModel(self.trainmodel)
+      self.pushButton_trainmodel.clicked.connect(self.open_trainmodel)
+      self.pushButton_trainmodel_test.clicked.connect(self.open_trainmodel_test)
+
+      # Train Controller Variables
+      self.pushButton_traincontrol.clicked.connect(self.open_traincontrol)
+      self.pushButton_traincontrol_test.clicked.connect(self.open_traincontrol_test)
+      
+
+      self.pushButton_start.clicked.connect(self.pause_timer)
 
       self.timer = QTimer()
       self.timer.setInterval(100)
@@ -52,20 +63,7 @@ class SystemWindow(QMainWindow, system.Ui_MainWindow):
       self.update_speed()
       self.horizontalSlider.valueChanged.connect(self.update_speed)
 
-# TODO: unconnect lines for module and add functions as indicated to open window
-      self.pushButton_trackcontrol.clicked.connect(self.open_TrackController)
-      # self.pushButton_trackcontrol_test.clicked.connect(self.open_TrackController_test)
 
-      self.trackmodel = TrackModelGUI()
-      self.trackModelTestUI = TrackModelTestUI()
-      self.pushButton_trackmodel.clicked.connect(self.open_TrackModel)
-      self.pushButton_trackmodel_test.clicked.connect(self.open_TrackModelTestUI)
-      
-      # self.pushButton_trainmodel.clicked.connect(self.open_TrainModel)
-      # self.pushButton_trainmodel_test.clicked.connect(self.open_TrainModel_test)
-
-      # self.pushButton_traincontrol.clicked.connect(self.open_TrainController)
-      # self.pushButton_traincontrol_test.clicked.connect(self.open_TrainController_test)
 
     def open_CTC(self):
       
@@ -92,6 +90,7 @@ class SystemWindow(QMainWindow, system.Ui_MainWindow):
 
     def open_trainmodel_test(self):
       self.trainmodel_test.show()
+
     def open_traincontrol(self):
       self.Trains.append(TrainController())
       self.Train = self.Trains[0]
@@ -103,7 +102,6 @@ class SystemWindow(QMainWindow, system.Ui_MainWindow):
       self.test_window = TrainCTRLTestUI(self.Trains[0])
       self.test_window.show()
 
-  
     def open_TrackModel(self):
       self.trackmodel.show()
 
