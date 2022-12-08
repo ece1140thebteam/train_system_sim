a3b6083a92516bd8f5fa92693198d55e9d86737c
Braden Change <bwchange@gmail.com>
2022-12-08 16:12:47 -0500

    yes

--- Train_Model/ui_form.py
+++ Train_Model/ui_form.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QLabel, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QWidget)
 
 class Ui_TrainModel(object):
     def setupUi(self, TrainModel):
@@ -27,7 +28,7 @@ class Ui_TrainModel(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.eBrake = QPushButton(self.centralwidget)
         self.eBrake.setObjectName(u"eBrake")
-        self.eBrake.setGeometry(QRect(210, 380, 201, 111))
+        self.eBrake.setGeometry(QRect(40, 370, 201, 111))
         self.eBrake.setCheckable(True)
         self.eBrake.setChecked(False)
         self.height = QLabel(self.centralwidget)
@@ -98,15 +99,15 @@ class Ui_TrainModel(object):
         self.grade.setGeometry(QRect(40, 240, 151, 16))
         self.enginefail = QPushButton(self.centralwidget)
         self.enginefail.setObjectName(u"enginefail")
-        self.enginefail.setGeometry(QRect(570, 290, 201, 51))
+        self.enginefail.setGeometry(QRect(300, 340, 201, 51))
         self.enginefail.setCheckable(True)
         self.brakefail = QPushButton(self.centralwidget)
         self.brakefail.setObjectName(u"brakefail")
-        self.brakefail.setGeometry(QRect(570, 360, 201, 51))
+        self.brakefail.setGeometry(QRect(300, 400, 201, 51))
         self.brakefail.setCheckable(True)
         self.signalfail = QPushButton(self.centralwidget)
         self.signalfail.setObjectName(u"signalfail")
-        self.signalfail.setGeometry(QRect(570, 430, 201, 51))
+        self.signalfail.setGeometry(QRect(300, 460, 201, 51))
         self.signalfail.setCheckable(True)
         self.enginefailure = QLabel(self.centralwidget)
         self.enginefailure.setObjectName(u"enginefailure")
@@ -132,6 +133,10 @@ class Ui_TrainModel(object):
         self.auth = QLabel(self.centralwidget)
         self.auth.setObjectName(u"auth")
         self.auth.setGeometry(QRect(440, 300, 181, 16))
+        self.trainSelect = QComboBox(self.centralwidget)
+        self.trainSelect.setObjectName(u"trainSelect")
+        self.trainSelect.setGeometry(QRect(650, 30, 141, 24))
+        self.trainSelect.setEditable(False)
         TrainModel.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(TrainModel)
         self.menubar.setObjectName(u"menubar")
